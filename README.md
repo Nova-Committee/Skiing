9ce55ac9d81a3a1d52951a7a448dac333a7d899e
cnlimiter <cnlimiter@163.com>
2025-05-07 01:44:04 +0800

    docs: add LICENSE and README files, remove unused imports
    - Add MIT License to the project
    - Create README.md with mod description and features
    - Remove unused imports in SkiingVillagers.java

---
+++ README.md
@@ -0,0 +1,14 @@
+## Skiing Mod
+You like skiing? Now you can do it in minecraft!
+
+##### This mod provides you with:
+*   Ski, Snowboard and Sled (5 per each wood type)
+*   Ski Sticks (you are much faster when you hold one in your mainhand and offhand)
+*   Snow Shovel (break a 3x3 area of snow at once)
+*   Dyeable Pullovers (to keep you warm)
+*   7 Paintings
+*   Ski Merchant (a villager that sells skis and other items from that mod, that spawns in a custom villager house)
+*   Config (for the skis entity and the snowlayering mechanic)
+*   Ski Rack (currently WIP)
+
+Get the mod [here](https://www.curseforge.com/minecraft/mc-mods/skiing2/)! 
--- src/main/java/committee/nova/mods/skiing/core/registry/SkiingVillagers.java
+++ src/main/java/committee/nova/mods/skiing/core/registry/SkiingVillagers.java
@@ -6,20 +6,16 @@ import com.google.common.collect.ImmutableSet;
 import net.minecraft.core.Holder;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.entity.ai.village.poi.PoiType;
-import net.minecraft.world.entity.ai.village.poi.PoiTypes;
 import net.minecraft.world.entity.npc.VillagerProfession;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.Objects;
 import java.util.function.Predicate;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class SkiingVillagers {
